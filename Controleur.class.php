1b6e4bb457c186c5f899ce84dca8f8d8d8fbb284
Jonathan Martel <jonathmartel@gmail.com>
2016-10-26 14:33:11 -0400

    commit initial

---
+++ Controleur.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Class Controleur
+ * Gère les requêtes HTTP
+ * 
+ * @author Jonathan Martel
+ * @version 1.0
+ * @update 2013-12-10
+ * @update 2016-01-22 : Adaptation du code aux standards de codage du département de TIM
+ * @license MIT
+ * @license http://opensource.org/licenses/MIT
+ * 
+ */
+
+class Controleur 
+{
+	
+		/**
+		 * Traite la requête
+		 * @return void
+		 */
+		public function gerer()
+		{
+			
+			switch ($_GET['requete']) {
+				case 'accueil':
+					$this->accueil();
+					break;
+				default:
+					$this->accueil();
+					break;
+			}
+		}
+		private function accueil()
+		{
+			$oVue = new Vue();
+			$oVue->afficheEntete();
+			$oVue->afficheAccueil();
+			$oVue->affichePied();
+		}
+		// Placer les méthodes du controleur.
+		
+		
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
