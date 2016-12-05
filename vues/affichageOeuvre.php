5aec31e9bbe26c8a035d0737bd093db29068d545
Nicolas <sylvestre.nico@gmail.com>
2016-12-04 21:19:31 -0500

    Amélioration affichage d'une oeuvre

--- vues/affichageOeuvre.php
+++ vues/affichageOeuvre.php
@@ -1,6 +1,7 @@
 ﻿<section id="oeuvreIndi">
 	<ul>
 <?php
+	//Affichage du titre de l'oeuvre
 	foreach($data[0] as $cle => $valeur)
 	{	
 		if($cle == "titre")
@@ -21,6 +22,7 @@
 	<li>Auteur(s):
 		<ul>
 	<?php
+		//Affichage du ou des auteurs de l'oeuvre
 		foreach($data as $auteur)
 		{
 			?>
@@ -58,11 +60,130 @@
 		</ul>
 	</li>
 	<?php
+	//Affichage des éléments restants du de l'oeuvre (ils sont présents)
 	foreach($data[0] as $cle => $valeur)
 	{	
-		if(($cle != "titre" && $cle != "prenomArtiste" && $cle != "nomArtiste" && $cle != "idArtiste" && $cle != "idOeuvre" && $cle != "titreVariante" && $cle != "collectif") && ($valeur != null ||$valeur != ""))
+		switch($cle)
 		{
-			echo "<li>".$cle." : ".$valeur."</li>";
+			case "dateFinProduction":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Date fin de production : ".$valeur."</li>";
+				}
+				break;
+				
+			case "dateAccession":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Date d'accession : ".$valeur."</li>";
+				}
+				break;
+				
+			case "nomCollection":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Nom de collection : ".$valeur."</li>";
+				}
+				break;
+				
+			case "nomCollection":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Nom de collection : ".$valeur."</li>";
+				}
+				break;
+			
+			case "modeAcquisition":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Mode d'acquisition : ".$valeur."</li>";
+				}
+				break;
+				
+			case "materiaux":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Materiaux : ".$valeur."</li>";
+				}
+				break;
+			
+			case "technique":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Technique : ".$valeur."</li>";
+				}
+				break;
+
+			case "dimensions":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Dimensions : ".$valeur."</li>";
+				}
+				break;
+				
+			case "parc":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Parc : ".$valeur."</li>";
+				}
+				break;
+				
+			case "batiment":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Batiment : ".$valeur."</li>";
+				}
+				break;
+				
+			case "adresseCivique":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Adresse civique : ".$valeur."</li>";
+				}
+				break;
+				
+			case "latitude":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Latitude : ".$valeur."</li>";
+				}
+				break;
+				
+			case "longitude":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Longitude : ".$valeur."</li>";
+				}
+				break;
+			
+			case "description":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Description : ".$valeur."</li>";
+				}
+				break;
+				
+			case "numeroAccession":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Numero d'accession : ".$valeur."</li>";
+				}
+				break;
+				
+			case "nomArrondissement":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Arrondissement : ".$valeur."</li>";
+				}
+				break;
+				
+			case "nomCategorie":
+				if($valeur != null && $valeur != "")
+				{
+					echo "<li>Categorie : ".$valeur."</li>";
+				}
+				break;
+				
 		}
 	}
 ?>
