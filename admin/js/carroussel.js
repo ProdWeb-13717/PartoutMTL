c006a188b57699061090ba098ccb33559d6f2a15
unknown <e1595029@FC411-MJPTCLB.cmaisonneuve.qc.ca>
2017-01-27 22:18:55 -0500

    remise finale

--- admin/js/carroussel.js
+++ admin/js/carroussel.js
@@ -13,59 +13,29 @@
     
     window.addEventListener("load", function(){                                                 // au chargement de la page
      
-        if(document.querySelector("#boutonAjoutCarroussel"))                                // 
+        if(document.querySelector("#boutonAjoutCarroussel"))
         { 
            document.querySelector("#boutonAjoutCarroussel").addEventListener("click", function(evt)
             {
+               
+               //valeur qui requeille les info du formulaire; ////////////////////////////////////
+               var choix = document.getElementsByClassName("choixPhoto");
+               
+               for(var c=0;c < choix.length;c++)
+               {
+                   if(choix[c].checked)
+                   {
+                       var choixPhoto = choix[c].value;
+                   }
+               }
+               
+               /*-- REQUÊTE GET ----------------------------------------------------------------------*/
 				
-                //console.log(evt.target.firstChild.nextSibling.nodeValue);
-				
-				
-			//valeur qui requeille les info du formulaire; ////////////////////////////////////
-				var choix = document.getElementsByClassName("choixPhoto");
-				for(var c=0;c < choix.length;c++)
-				{
-					if(choix[c].checked)
-					{
-						var choixPhoto 		= choix[c].value;
-					}
-				}
-				//console.log(lien);
-			///////////////////////////////////////////////////////////////////////////////////
-				if((titre != null && choixPhoto != null) /*&& titre != ""*/)
-				{
-					//document.getElementById("carrousselAjoutForm").value = true;
-					document.getElementById("carrousselAjoutForm").action = "index.php?requete=ajouterImageCarroussel";
-					//document.getElementById("carrousselAjoutForm").submit;
-					// console.log(data);
-					/*-- REQUÊTE AJAX -------------------------------------------------------------*/
-	/*
-				var titre 			= document.getElementById("carrousselAjoutTitre").value;
-				var lienImage 		= "../images/photo_" + choixPhoto + ".jpg";
-				var lien 			= "index.php?requete=afficheOeuvre&idOeuvre=" + choixPhoto;
-				
-					var data = JSON.stringify	({
-													titre 		: titre,
-													urlLien 	: lien,
-													urlPhoto 	: lienImage
-												});
-					var xhr = new XMLHttpRequest();                                                 // nouvelle requête
-					
-					xhr.open("GET", "index.php?requete=ajouterImageCarroussel&titre=" + titre + "&idPhoto=" + choixPhoto) ;
-					xhr.setRequestHeader();
-					
-					xhr.addEventListener("load", function(e){
-						
-						document.getElementById("carrousselAjoutTitre").value = "";
-						window.location.href = "./index.php?requete=affichage";               
-						
-					});
-					
-					xhr.send();                                                        // envoie la requête et les datas en POST
-						*/						
-				}
+               if(choixPhoto != null)
+               {
+                   document.getElementById("carrousselAjoutForm").action = "index.php?requete=ajouterImageCarroussel&id=" + choixPhoto;
+               }
             });  
-        
         }
     })
 })();
